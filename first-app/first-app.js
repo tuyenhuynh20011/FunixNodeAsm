a7594461ce5e1981d8e14bf9628e90e91863e8c7
tuyenhuynh20011 <83766834+tuyenhuynh20011@users.noreply.github.com>
2022-08-29 12:54:28 +0700

    lab1.3

--- first-app/first-app.js
+++ first-app/first-app.js
@@ -2,6 +2,12 @@ const http= require('http')
 
 const sever=http.createServer((req, res)=> {
     console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Myfirst page </title></head>');
+    res.write('<body><h1>My first server</h1></body>');
+    res.write('</html>');
+    res.end();
 })
 
 sever.listen(3000);
