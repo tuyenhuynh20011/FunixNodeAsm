cdd62b0943482cdf703a493ef0d2e021d73366fe
tuyenhuynh20011 <83766834+tuyenhuynh20011@users.noreply.github.com>
2022-08-29 13:03:55 +0700

    lab1.4

--- first-app/first-app.js
+++ first-app/first-app.js
@@ -1,11 +1,21 @@
 const http= require('http')
 
 const sever=http.createServer((req, res)=> {
-    console.log(req);
+    const url=req.url;
+    if(url === '/')
+    {
+
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>enter messenge</title></head>');
+        res.write('<body><form action="/messege" method="POST"><input type="text" name="messege"><button type="submit">send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Myfirst page </title></head>');
-    res.write('<body><h1>My first server</h1></body>');
+    res.write('<body><h1>My first server have path / </h1></body>');
     res.write('</html>');
     res.end();
 })
