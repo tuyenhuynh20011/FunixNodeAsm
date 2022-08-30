209ebdd60e91767d05e484072e68586795a451e9
tuyenhuynh20011 <83766834+tuyenhuynh20011@users.noreply.github.com>
2022-08-30 09:42:04 +0700

    cmlab1.7

--- first-app/first-app.js
+++ first-app/first-app.js
@@ -1,40 +1,8 @@
-const http= require('http')
-const fs= require('fs');
-const sever=http.createServer((req, res)=> {
-    const url=req.url;
-    if(url === '/')
-    {
+const http= require('http');
+const routes=require('./routes.js');
 
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>enter messenge</title></head>');
-        res.write('<body><form action="/messege" method="POST"><input type="text" name="messege"><button type="submit">send</button></form></body>');
-        res.write('</html>');
-        return res.end();
+const sever=http.createServer(routes.handler)
 
-    }
-    if(url === '/message' && method === 'POST')
-    {
-        const body=[] ;
-        req.on('data', (chunk)=> {
-            console.log(chunk);
-            body.push(chunk);
-        })
-        req.on('end', ()=>{
-            const parseBody=Buffer.concat(body).toString();
-            console.log(parseBody);
-        })
-        fs.writeFileSync('message.txt','DUMMY');
-        res.statusCode=302;
-        res.setHeader('location','/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Myfirst page </title></head>');
-    res.write('<body><h1>My first server have path / </h1></body>');
-    res.write('</html>');
-    res.end();
-})
+console.log(routes.someText)
 
 sever.listen(3000);
