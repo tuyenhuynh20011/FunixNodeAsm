fb03c32aeab6bad81c87b9d17629983a00cb7b16
tuyenhuynh20011 <83766834+tuyenhuynh20011@users.noreply.github.com>
2022-09-07 12:06:54 +0700

    lab2.11

--- first-app/routes/shop.js
+++ first-app/routes/shop.js
@@ -1,10 +1,9 @@
 const express = require("express");
-
+const path = require("path");
 const router = express.Router();
 
-
 router.get("/", (req, res, next) => {
-    res.send('<h1>welcome</h1>')
-})
+  res.sendFile(path.join(__dirname,'../','views','shop.html'));
+});
 
-module.exports =router
+module.exports = router;
