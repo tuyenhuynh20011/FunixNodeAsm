dc0b3cc06a3b4dd2171f1236aeb25854376c214c
tuyenhuynh20011 <83766834+tuyenhuynh20011@users.noreply.github.com>
2022-09-07 12:19:35 +0700

    lab2.13

--- first-app/routes/admin.js
+++ first-app/routes/admin.js
@@ -1,9 +1,11 @@
 const express = require("express");
+
+const rootDir= require('../util/path')
+
 const path = require("path");
 const router = express.Router();
-
 router.use("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname,'../','views','admin.html'));
+  res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 router.post("/product", (req, res, next) => {
   console.log(req.body);
