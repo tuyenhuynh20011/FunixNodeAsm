3f6a7b5e9d519c30cb1fde966301de02b635a921
tuyenhuynh20011 <83766834+tuyenhuynh20011@users.noreply.github.com>
2022-12-19 14:27:23 +0700

    .
    .

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,6 +6,10 @@ const PDFDocument = require("pdfkit");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
+
+const ITEMS_PER_PAGE = 2
+
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then((products) => {
@@ -41,12 +45,27 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page= +req.query.page || 1 ;
+  let totalItems;
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems=numProducts;
+      return Product.find()
+      .skip((page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE)
+    })
     .then((products) => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
         path: "/",
+        currentPage: page,
+        hasNextPage:ITEMS_PER_PAGE * page < totalItems,
+        hasPreviosPage:page > 1 ,
+        nextPage: page+1,
+        previousPage:page -1 ,
+        lastPage:Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch((err) => {
